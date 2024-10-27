ea6ed610ae570adfe1f84a73fbfa96f58baef082
Krishna Perumal R <krishnaperumal26@gmail.com>
2024-10-27 21:18:03 +0530

    added basic class layout

---
+++ src/models/ParkingLot.java
@@ -0,0 +1,36 @@
+package models;
+
+import java.util.List;
+
+public class ParkingLot {
+    private List<ParkingFloor> parkingFloorList;
+    private List<Gate> gates;
+    private int capacity;
+
+    public List<ParkingFloor> getParkingFloorList() {
+        return parkingFloorList;
+    }
+
+    public void setParkingFloorList(List<ParkingFloor> parkingFloorList) {
+        this.parkingFloorList = parkingFloorList;
+    }
+
+    public List<Gate> getGates() {
+        return gates;
+    }
+
+    public void setGates(List<Gate> gates) {
+        this.gates = gates;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+
+
+}
---
+++ src/models/ParkingSpot.java
@@ -0,0 +1,12 @@
+package models;
+
+import java.util.List;
+
+public class ParkingSpot {
+    private int number;
+    private List<VehicleType> vehicleType;
+    private SpotStatus spotStatus;
+    private Vehicle vehicle;
+
+
+}
